6f1b613d375a8692e1d99bf1a417b769a0b8ee50
Lincoln <Lincoln@usa.com>
2021-04-17 15:36:35 +0800

    docs:修复main.py的代码格式

--- web/main.py
+++ web/main.py
@@ -16,67 +16,67 @@ import web.dataIndicatorsHandler as dataIndicatorsHandler
 import web.base as webBase
 
 
-class Application( tornado.web.Application ) :
-	def __init__( self ) :
+class Application(tornado.web.Application):
+	def __init__(self):
 		handlers = [
 			# 设置路由
-			(r"/" , HomeHandler) ,
-			(r"/test" , TestHandler) ,  # 测试页面，做写js 测试。
-			(r"/test2" , Test2Handler) ,  # 测试页面，做写js 测试。
+			(r"/", HomeHandler),
+			(r"/test", TestHandler),  # 测试页面，做写js 测试。
+			(r"/test2", Test2Handler),  # 测试页面，做写js 测试。
 			# 使用datatable 展示报表数据模块。
-			(r"/stock/api_data" , dataTableHandler.GetStockDataHandler) ,
-			(r"/stock/data" , dataTableHandler.GetStockHtmlHandler) ,
+			(r"/stock/api_data", dataTableHandler.GetStockDataHandler),
+			(r"/stock/data", dataTableHandler.GetStockHtmlHandler),
 			# 数据修改dataEditor。
-			(r"/data/editor" , dataEditorHandler.GetEditorHtmlHandler) ,
-			(r"/data/editor/save" , dataEditorHandler.SaveEditorHandler) ,
+			(r"/data/editor", dataEditorHandler.GetEditorHtmlHandler),
+			(r"/data/editor/save", dataEditorHandler.SaveEditorHandler),
 			# 获得股票指标数据。
-			(r"/data/indicators" , dataIndicatorsHandler.GetDataIndicatorsHandler) ,
+			(r"/data/indicators", dataIndicatorsHandler.GetDataIndicatorsHandler),
 		]
 		settings = dict(  # 配置
-				template_path = os.path.join( os.path.dirname( __file__ ) , "templates" ) ,
-				static_path = os.path.join( os.path.dirname( __file__ ) , "static" ) ,
-				xsrf_cookies = False ,  # True,
+				template_path = os.path.join(os.path.dirname(__file__), "templates"),
+				static_path = os.path.join(os.path.dirname(__file__), "static"),
+				xsrf_cookies = False,  # True,
 				# cookie加密
-				cookie_secret = "027bb1b670eddf0392cdda8709268a17b58b7" ,
-				debug = True ,
+				cookie_secret = "027bb1b670eddf0392cdda8709268a17b58b7",
+				debug = True,
 		)
-		super( Application , self ).__init__( handlers , **settings )
+		super(Application, self).__init__(handlers, **settings)
 		# Have one global connection to the blog DB across all handlers
 		self.db = torndb.Connection(
-				charset = "utf8" , max_idle_time = 3600 , connect_timeout = 1000 ,
-				host = common.MYSQL_HOST , database = common.MYSQL_DB ,
-				user = common.MYSQL_USER , password = common.MYSQL_PWD )
+				charset = "utf8", max_idle_time = 3600, connect_timeout = 1000,
+				host = common.MYSQL_HOST, database = common.MYSQL_DB,
+				user = common.MYSQL_USER, password = common.MYSQL_PWD)
 
 
 # 首页handler。
-class HomeHandler( webBase.BaseHandler ) :
+class HomeHandler(webBase.BaseHandler):
 	@gen.coroutine
-	def get( self ) :
-		self.render( "index.html" , entries = "hello" , leftMenu = webBase.GetLeftMenu( self.request.uri ) )
+	def get(self):
+		self.render("index.html", entries = "hello", leftMenu = webBase.GetLeftMenu(self.request.uri))
 
 
-class TestHandler( webBase.BaseHandler ) :
+class TestHandler(webBase.BaseHandler):
 	@gen.coroutine
-	def get( self ) :
-		self.render( "test.html" , entries = "hello" , leftMenu = webBase.GetLeftMenu( self.request.uri ) )
+	def get(self):
+		self.render("test.html", entries = "hello", leftMenu = webBase.GetLeftMenu(self.request.uri))
 
 
-class Test2Handler( webBase.BaseHandler ) :
+class Test2Handler(webBase.BaseHandler):
 	@gen.coroutine
-	def get( self ) :
-		self.render( "test2.html" , entries = "hello" , leftMenu = webBase.GetLeftMenu( self.request.uri ) )
+	def get(self):
+		self.render("test2.html", entries = "hello", leftMenu = webBase.GetLeftMenu(self.request.uri))
 
 
-def main( ) :
-	tornado.options.parse_command_line( )
-	http_server = tornado.httpserver.HTTPServer( Application( ) )
+def main():
+	tornado.options.parse_command_line()
+	http_server = tornado.httpserver.HTTPServer(Application())
 	port = 9999
-	http_server.listen( port )
+	http_server.listen(port)
 	# tornado.options.options.logging = "debug"
-	tornado.options.parse_command_line( )
+	tornado.options.parse_command_line()
 
-	tornado.ioloop.IOLoop.current( ).start( )
+	tornado.ioloop.IOLoop.current().start()
 
 
-if __name__ == "__main__" :
-	main( )
+if __name__ == "__main__":
+	main()
